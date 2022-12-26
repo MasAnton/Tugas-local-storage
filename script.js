68f09a71eeb73ab281c4e553961153d18715b732
MasAnton <antonprayogo002@gmail.com>
2022-12-27 00:05:19 +0700

    Tinggal Eksekusi Tugas

---
+++ script.js
@@ -0,0 +1,8 @@
+let loginButton = document.getElementById("loginButton");
+let loginPage = document.getElementById("loginPage");
+let usernameInput = document.getElementById("usernameInput");
+let passwordInput = document.getElementById("passwordInput");
+
+let login = () => {
+  loginPage.style.display = "none";
+};
